9c40168a6fa6e824c1795af3d1f3888b8e5a4fda
Darian Tavana <datavana@gmail.com>
2019-04-09 15:43:30 +0000

    routes now working

--- routes/api/settings.js
+++ routes/api/settings.js
@@ -2,8 +2,7 @@ const express = require('express')
   , router = express.Router()
 
 router.get('/', (req, res) => {
-    console.log(1);
-    res.send(200);
+  res.send(200)
 })
 
 module.exports = router
